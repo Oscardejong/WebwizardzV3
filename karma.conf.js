412009eea4ed91702514ca0806e2842d332f9af9
I541637 <oscar.dejong@student.fontys.nl>
2025-06-01 21:03:28 +0200

    Werkende unittests

--- karma.conf.js
+++ karma.conf.js
@@ -55,7 +55,14 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://www.npmjs.com/search?q=keywords:karma-launcher
-    browsers: ['ChromeHeadless'],
+        browsers: ['ChromeHeadlessNoSandbox'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-gpu', '--disable-dev-shm-usage', '--disable-setuid-sandbox', '--headless', '--remote-debugging-port=9222']
+      }
+    },
+
 
 
     // Continuous Integration mode
